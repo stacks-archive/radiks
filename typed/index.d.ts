86e5fea43cd64a48f8b7f3f8657862e0bf2e521c
pradel <pradel.leo@gmail.com>
2019-05-20 17:23:36 +0200

    Delete typed folder

--- typed/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Model from './model';
-import UserGroup from './models/user-group';
-import User from './models/user';
-import { configure, getConfig } from './config';
-import GroupMembership from './models/group-membership';
-import GroupInvitation from './models/group-invitation';
-import Central from './central';
-export { Model, configure, getConfig, UserGroup, GroupMembership, User, GroupInvitation, Central, };
--- typed/streamer.d.ts
+++
@@ -1,9 +0,0 @@
-import EventEmitter from 'wolfy87-eventemitter';
-export default class Streamer {
-    static initialized: boolean;
-    static socket: WebSocket;
-    static emitter: EventEmitter;
-    static init(): WebSocket;
-    static addListener(callback: (args: any[]) => void): void;
-    static removeListener(callback: Function): void;
-}
