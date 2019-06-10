43d25759efe2ddb7c2b654bc9c0d5e191c903d0b
Hank Stoever <hstove@gmail.com>
2019-06-09 17:51:28 -0700

    remove /typed (for merge conflicts)

--- typed/central.d.ts
+++
@@ -1,9 +0,0 @@
-declare class Central {
-    static save(key: string, value: Record<string, any>): Promise<any>;
-    static get(key: string): Promise<any>;
-    static makeSignature(key: string): {
-        username: string;
-        signature: string;
-    };
-}
-export default Central;
